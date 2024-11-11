ef39644977432806f74228005abcf47af58415f7
ortofone24 <ortofone24@gmail.com>
2024-11-11 20:34:00 +0100

    remove comments

--- src/Services/TodoList/TodoList.Api/Program.cs
+++ src/Services/TodoList/TodoList.Api/Program.cs
@@ -10,14 +10,9 @@ using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddInfrastructureServices(builder.Configuration);
-//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
-
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetAllTasksQueryHandler).Assembly));
-//builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.Load("TodoList.Application")));
+
 var applicationAssembly = typeof(TodoList.Application.AssemblyReference).Assembly;
 builder.Services.AddValidatorsFromAssembly(applicationAssembly);
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
@@ -38,9 +33,6 @@ builder.Services.AddCors(options =>
                .AllowAnyHeader();
     });
 });
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
 
 var app = builder.Build();
 app.UseDatabaseInitialization();
